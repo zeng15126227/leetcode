a00fce4118bddccc98f9ee104987371d46f743a1
zengxz8 <zengxz8@chinaunicom.cn>
2020-10-21 18:27:50 +0800

    20201021 18：53

---
+++ editor/cn/[96]不同的二叉搜索树.py
@@ -0,0 +1,23 @@
+# 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？ 
+# 
+#  示例: 
+# 
+#  输入: 3
+# 输出: 5
+# 解释:
+# 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+# 
+#    1         3     3      2      1
+#     \       /     /      / \      \
+#      3     2     1      1   3      2
+#     /     /       \                 \
+#    2     1         2                 3 
+#  Related Topics 树 动态规划 
+#  👍 862 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def numTrees(self, n: int) -> int:
+
+# leetcode submit region end(Prohibit modification and deletion)

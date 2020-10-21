67bb495d54571882a6dd76e172c047622a990cda
zengxz8 <zengxz8@chinaunicom.cn>
2020-10-21 15:53:53 +0800

    20201021 15：53

--- editor/cn/[85]最大矩形.py
+++ editor/cn/[85]最大矩形.py
@@ -17,5 +17,52 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maximalRectangle(self, matrix: list) -> int:
-        return 0
+
+
+        if len(matrix) == 0:
+            return 0
+
+
+        maxarea = 0
+
+        dp = [0] * len(matrix[0])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                #行数据进行填充
+                dp[j]=dp[j]+1 if matrix[i][j]=="1" else 0
+            maxarea = max(maxarea,self.largestRectangleArea(dp))
+
+        return maxarea
+
+    def largestRectangleArea(self, heights: list) -> int:
+
+        if not heights:
+            return 0
+        #头尾添加哨兵
+        heights = [-1] + heights + [-1]
+        res_list = [0 for _ in heights]
+        l = len(heights)
+        #栈中添加哨兵
+        stack = [0]
+
+        for i in range(1,l):
+            #栈中元素为递增，下一个元素大于栈顶则入栈
+            if heights[stack[-1]] < heights[i]:
+                stack.append(i)
+            #下一个元素小于栈顶，栈顶出栈，出栈的元素左右边界可确定
+            elif heights[stack[-1]] > heights[i]:
+                while heights[stack[-1]]>heights[i]:
+                    out = stack.pop()
+                    res_list[out]=(i-stack[-1]-1)*heights[out]
+                stack.append(i)
+            #下一个元素等于栈顶，栈顶元素进行替换，为了卡住下一个栈元素的左边界
+            else:
+                stack[-1] = i
+
+        return max(res_list)
 # leetcode submit region end(Prohibit modification and deletion)
+if __name__ == "__main__":
+    param=[
+ ]
+    res = Solution().maximalRectangle(param)
+    print(res)
